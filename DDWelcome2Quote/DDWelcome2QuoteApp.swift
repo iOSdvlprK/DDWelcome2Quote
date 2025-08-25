33bd68bfc698a03a5cdfdad562bbfc3f4e49d666
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-08-25 22:04:39 +0900

    Initial Commit

---
+++ DDWelcome2Quote/DDWelcome2QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDWelcome2QuoteApp.swift
+//  DDWelcome2Quote
+//
+//  Created by joe on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct DDWelcome2QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
